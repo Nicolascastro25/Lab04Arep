e1e8a13f6c558bfbd82a0c90d209f554a4b15f05
unknown <jaime.cacuna@labinfo.is.escuelaing.edu.co>
2022-10-05 18:50:58 -0500

    Se realiza la funcion get con spark y mongoDB

--- src/main/java/co/edu/escuelaing/lab04arep/MongoDBConnection.java
+++ src/main/java/co/edu/escuelaing/lab04arep/MongoDBConnection.java
@@ -4,8 +4,10 @@
  */
 package co.edu.escuelaing.lab04arep;
 
-import com.mongodb.MongoClient;
-import com.mongodb.MongoClientURI;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import java.util.Date;
 import org.bson.Document;
@@ -16,25 +18,34 @@ import org.bson.Document;
  */
 public class MongoDBConnection {
 
-    private MongoClient mongoClient;
     private MongoDatabase mongodb;
-    private MongoClientURI uriMongo;
+    private MongoClient mongoClient;
+    private String uriMongo;
 
-    public MongoDBConnection() {
-        uriMongo = new MongoClientURI("mongodb+srv://jaimecacuna:<password>@cluster0.eju0yt7.mongodb.net/?retryWrites=true&w=majority");
-        mongoClient = new MongoClient(uriMongo);
-        mongodb = mongoClient.getDatabase("Cluster0");
+    public MongoDBConnection() {        
+        uriMongo = "mongodb://jaimecacuna:nicolasarep@ac-rasowq5-shard-00-00.k3zsmoc.mongodb.net:27017,ac-rasowq5-shard-00-01.k3zsmoc.mongodb.net:27017,ac-rasowq5-shard-00-02.k3zsmoc.mongodb.net:27017/?ssl=true&replicaSet=atlas-f3y7wy-shard-0&authSource=admin&retryWrites=true&w=majority";
+        mongoClient = MongoClients.create(uriMongo);
+        mongodb = mongoClient.getDatabase("cluster0");
     }
 
     public void insertInfoDB(String info) {
         if (mongodb.getCollection("mensajes") == null) {
             mongodb.createCollection("mensajes");
         }
-        mongodb.createCollection("mensajes");
         Document document = new Document();
         document.append("Fecha", new Date().toString());
         document.append("Mensaje", info);
         mongodb.getCollection("mensajes").insertOne(document);
+    }
+
+    public String getInfoDB() {
+        MongoCollection<Document> collection = mongodb.getCollection("mensajes");
+        String message = "";
+        FindIterable<Document> iterable = collection.find();
+        for (Document document : iterable) {
+            message += "_Id:_ " + document.get("_id") + " _Fecha:_ " + document.get("Fecha").toString() + " _Mensaje:_ " +  document.get("Mensaje").toString() + "<br><br>";
+        }
+        return message;
     }
 
 }
--- src/main/java/co/edu/escuelaing/lab04arep/SparkWebServer.java
+++ src/main/java/co/edu/escuelaing/lab04arep/SparkWebServer.java
@@ -2,8 +2,11 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
  */
-
 package co.edu.escuelaing.lab04arep;
+
+import spark.Request;
+import spark.Response;
+import static spark.Spark.*;
 
 /**
  *
@@ -11,7 +14,44 @@ package co.edu.escuelaing.lab04arep;
  */
 public class SparkWebServer {
 
+    private static MongoDBConnection mongoDb = new MongoDBConnection();
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        port(getPort());
+        get("/mensajes", (req, res) -> traerInfoColeccion(req, res));
+
     }
+
+    private static String traerInfoColeccion(Request req, Response res) {
+        String pageContent
+                = "<!DOCTYPE html>"
+                + "<html>"
+                + "<body>"
+                + "<h2>MongoDB</h2>"
+                + "<br>"
+                + "<h4>La info de la base de datos es: </h4>"
+                + "<br>"
+                + "<br>"
+                + getMessages()
+                + "</body>"
+                + "</html>";
+        return pageContent;
+    }
+
+    private static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 4567;
+    }
+
+    private static String insertMessage() {
+        return "hola";
+    }
+
+    private static String getMessages() {
+        String content = mongoDb.getInfoDB();
+        return content;
+    }
+
 }
